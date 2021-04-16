f78f7901822895f20289e4e1bf87f328292846ec
Globox1997 <mitschke11@web.de>
2021-04-16 21:07:39 +0200

    initial commit

---
+++ src/main/java/net/tooltiprareness/mixin/MusicDiscItemMixin.java
@@ -0,0 +1,30 @@
+package net.tooltiprareness.mixin;
+
+import java.util.List;
+
+import org.jetbrains.annotations.Nullable;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.At;
+
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.minecraft.client.item.TooltipContext;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.MusicDiscItem;
+import net.minecraft.text.Text;
+import net.minecraft.text.TranslatableText;
+import net.minecraft.world.World;
+
+@Environment(EnvType.CLIENT)
+@Mixin(MusicDiscItem.class)
+public class MusicDiscItemMixin {
+
+    @Inject(method = "appendTooltip", at = @At("HEAD"))
+    private void appendTooltipMixin(ItemStack stack, @Nullable World world, List<Text> tooltip, TooltipContext context,
+            CallbackInfo info) {
+        tooltip.add(new TranslatableText("item.tooltiprareness.rare_item.tooltip"));
+    }
+
+}
